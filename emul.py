8bf13732e379e42dc593203617a1a7e85c37dd41
Cwits <portablejoe@LAPTOP-CE6JCDFH.localdomain>
2026-02-19 21:25:22 +0100

    it's ok

--- emul.py
+++ emul.py
@@ -3,6 +3,7 @@ from aiohttp import web
 import socket
 
 WS_CLIENTS = set()
+TCP_CLIENTS = set()
 
 TCP_HOST = "0.0.0.0"
 TCP_PORT = 9000
@@ -40,6 +41,8 @@ async def handle_tcp_client(reader, writer):
     addr = writer.get_extra_info("peername")
     print("TCP client connected:", addr)
 
+    TCP_CLIENTS.add(writer)
+
     try:
         while True:
             header = await recv_exact(reader, 8)
@@ -56,14 +59,34 @@ async def handle_tcp_client(reader, writer):
                 print(f"broken magic {magic}")
                 print(f"received broken magic in header {header}")
 
+            data = await recv_exact(reader, dataSize)
+            if data is None:
+                break
+
+                # print(f"received {len(fb)} bytes")
+
+            frameEnd = await recv_exact(reader, 3)
+            if frameEnd != b'\xAD\xBE\xCF':
+                print(f"received broken magic after data {frameEnd}")
+                continue
+
+            # рассылка браузерам
+            for ws in list(WS_CLIENTS):
+                if not ws.closed:
+                    await ws.send_bytes(header)
+                    await ws.send_bytes(data)
+                    await ws.send_bytes(frameEnd)
+                    # await ws.send_bytes(fb)
+            
+            '''
             if cmdType == 1:
-                print(f"received framebuffer data, len={dataSize}")
+                # print(f"received framebuffer data, len={dataSize}")
 
                 fb = await recv_exact(reader, dataSize)
                 if fb is None:
                     break
 
-                print(f"received {len(fb)} bytes")
+                # print(f"received {len(fb)} bytes")
 
                 frameEnd = await recv_exact(reader, 3)
                 if frameEnd != b'\xAD\xBE\xCF':
@@ -81,16 +104,35 @@ async def handle_tcp_client(reader, writer):
                         await ws.send_bytes(fb)
                         await ws.send_bytes(frameEnd)
                         # await ws.send_bytes(fb)
-
+            elif cmdType == 2:
+                #button data
+                continue
+            elif cmdType == 4:
+                #touchstrip data
+                continue
+            elif cmdType == 6:
+                #pad data
+                continue
+            '''
     except Exception as e:
         print("TCP error:", addr, e)
 
     finally:
+        TCP_CLIENTS.remove(writer)
         writer.close()
         await writer.wait_closed()
         print("TCP connection closed:", addr)
 
+async def send_to_tcp(payload: bytes):
+    # packet = bytes([cmd]) + len(payload).to_bytes(4, "little") + payload
 
+    for w in list(TCP_CLIENTS):
+        try:
+            # w.write(packet)
+            w.write(payload)
+            await w.drain()
+        except:
+            TCP_CLIENTS.remove(w)
 
 # ---------- websocket ----------
 async def ws_handler(request):
@@ -99,13 +141,22 @@ async def ws_handler(request):
 
     WS_CLIENTS.add(ws)
     print("Browser connected")
+    conndata = bytes([7, 1, 0, 0, 0, 0, 0, 0]) #system(7), push connected(1)
+    await send_to_tcp(conndata)
+    data = bytes([1])
+    await send_to_tcp(data)
 
     try:
         async for msg in ws:
             if msg.type == web.WSMsgType.BINARY:
                 # здесь можно отправлять события обратно на TCP клиент
-                print(f"Received data from ws {msg}")
+                # print(f"Received data from ws {msg}")
+                await send_to_tcp(msg.data)
     finally:
+        conndata = bytes([7, 1, 0, 0, 0, 0, 0, 0]) #system(7), push connected(1)
+        disconndata = bytes([2]) #system(7), push disconnected(2)
+        await send_to_tcp(conndata)
+        await send_to_tcp(disconndata)
         WS_CLIENTS.remove(ws)
         print("Browser disconnected")
 
--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 const WIDTH = 960;
 const HEIGHT = 160;          // реальные данные
-const CANVAS_HEIGHT = 180;   // размер канваса
+const CANVAS_HEIGHT = 160;   // размер канваса
 const ROW_LENGTH = 2048;
 const DISPLAY_WIDTH_BYTES = 1920;
 
 const pattern = [0xE7, 0xF3, 0xE7, 0xFF];
+
+const DEFAULT_COLOR_PALLETE = {
+    0:[0,0,0],         1:[30,30,30],     2:[127,127,127],   3:[255,255,255],
+    4:[255,76,76],     5:[255,0,0],      6:[89,0,0],        7:[25,0,0],
+    8:[255,189,108],   9:[255,84,0],     10:[89,29,0],      11:[25,8,0],
+    12:[255,255,76],   13:[255,255,0],   14:[89,89,0],      15:[25,25,0],
+    16:[136,255,76],   17:[84,255,0],    18:[29,89,0],      19:[8,25,0],
+    20:[76,255,76],    21:[0,255,0],     22:[0,89,0],       23:[0,25,0],
+    24:[76,255,136],   25:[0,255,84],    26:[0,89,29],      27:[0,25,8],
+    28:[76,255,255],   29:[0,255,255],   30:[0,89,89],      31:[0,25,25],
+    32:[76,136,255],   33:[0,84,255],    34:[0,29,89],      35:[0,8,25],
+    36:[76,76,255],    37:[0,0,255],     38:[0,0,89],       39:[0,0,25],
+    40:[136,76,255],   41:[84,0,255],    42:[29,0,89],      43:[8,0,25],
+    44:[255,76,255],   45:[255,0,255],   46:[89,0,89],      47:[25,0,25],
+    48:[255,76,136],   49:[255,0,84],    50:[89,0,29],      51:[25,0,8],
+    52:[255,21,0],     53:[153,53,0],    54:[121,81,0],     55:[67,100,0], 
+    56:[0,89,0],       57:[0,84,53],     58:[0,81,121],     59:[0,53,153], 
+    60:[0,0,255],      61:[53,0,153],    62:[121,0,81],     63:[153,0,53],
+    64:[255,0,0],      65:[189,255,45],  66:[138,255,0],    67:[0,255,0], 
+    68:[0,255,189],    69:[0,189,255],   70:[0,138,255],    71:[0,0,255], 
+    72:[189,0,255],    73:[255,0,189],   74:[255,0,138],    75:[255,189,189], 
+    76:[255,234,189],  77:[255,255,189], 78:[234,255,189],  79:[189,255,189], 
+    80:[189,255,234],  81:[189,255,255], 82:[189,234,255],  83:[189,189,255], 
+    84:[234,189,255],  85:[255,189,255], 86:[255,189,234],  87:[255,102,102], 
+    88:[255,153,102],  89:[255,255,102], 90:[153,255,102],  91:[102,255,102], 
+    92:[102,255,153],  93:[102,255,255], 94:[102,153,255],  95:[102,102,255], 
+    96:[153,102,255],  97:[255,102,255], 98:[255,102,153],  99:[204,0,0], 
+    100:[204,102,0],   101:[204,204,0],  102:[102,204,0],   103:[0,204,0], 
+    104:[0,204,102],   105:[0,204,204],  106:[0,102,204],   107:[0,0,204], 
+    108:[102,0,204],   109:[204,0,204],  110:[204,0,102],   111:[153,153,153], 
+    112:[115,115,115], 113:[77,77,77],   114:[38,38,38],    115:[255,64,64], 
+    116:[255,128,64],  117:[255,255,64], 118:[128,255,64],  119:[64,255,64], 
+    120:[64,255,128],  121:[64,255,255], 122:[64,128,255],  123:[64,64,255], 
+    124:[128,64,255],  125:[255,64,255], 126:[255,64,128],  127:[0,0,0]
+  };
+  
+
+//rgb buttons: 102-109, 20-27, 29, 36-43, 60-61, 85, 86, 89
+const rgbButtons = [102, 103, 104, 105, 106, 107, 108, 109,
+                    20, 21, 22, 23, 24, 25, 26, 27, 29, 
+                    36, 37, 38, 39, 40, 41, 42, 43,
+                    60, 61, 85, 86, 89];
 
 const ws = new WebSocket("ws://" + location.host + "/ws");
 ws.binaryType = "arraybuffer";
@@ -26,7 +68,8 @@ function processPacket(packet) {
       Button = 2,
       Encoder = 3,
       Touchstrip = 4,
-      Pedal = 5
+      Pedal = 5,
+      Pad = 6
     */
     if(type == 1) {
         //display frame buffer
@@ -62,20 +105,67 @@ function processPacket(packet) {
             }
         }
 
-        // если canvas выше буфера — можно зачистить низ
-        for (let y = HEIGHT; y < CANVAS_HEIGHT; y++) {
-            for (let x = 0; x < WIDTH; x++) {
-                const i = (y * WIDTH + x) * 4;
-                out[i] = 0;
-                out[i+1] = 0;
-                out[i+2] = 0;
-                out[i+3] = 255;
-            }
-        }
-
         ctx.putImageData(img, 0, 0);
     } else if(type == 2) {
-        //...
+        //... button data
+        const data = packet.data;
+        
+        const btnnum = data[0];
+        const btn = "cc"+btnnum;
+        const color = data[1];
+
+        const el = document.getElementById(btn);
+        if(!el) {
+            console.log("cant find element " + btn);
+            return;
+        }
+        if(rgbButtons.includes(btnnum)) {
+            //its rgb
+            if(color == 0) {
+                // el.style.backgroundColor = "Black";
+                el.style.setProperty("--pad-color", `rgb(${0},${0},${0})`);
+            } else { 
+                // el.style.backgroundColor = "Red";
+                const rgb = DEFAULT_COLOR_PALLETE[color];
+                const r = rgb[0];
+                const g = rgb[1];
+                const b = rgb[2];
+                // el.style.backgroundColor = 'rgb(' + [r,g,b].join(',') + ')';
+                el.style.setProperty("--pad-color", `rgb(${r},${g},${b})`);
+            }
+        } else {
+            if(color == 0) {
+                // el.style.backgroundColor = "Black";
+                el.style.setProperty("--pad-color", `rgb(${0},${0},${0})`);
+            } else {
+                // el.style.backgroundColor = "White";
+                el.style.setProperty("--pad-color", `rgb(${255},${255},${255})`);
+            }   
+        }
+
+    } else if(type == 4) {
+        //touchstrip data
+    } else if(type == 6) {
+        //pad data
+        const data = packet.data;
+        
+        const btnnum = data[0];
+        const btn = "nn"+btnnum;
+        const color = data[1];
+
+        const el = document.getElementById(btn);
+        if(color == 0) {
+            // el.style.backgroundColor = "Black";
+            el.style.setProperty("--pad-color", 'rgb(${0},${0},${0})');
+        } else { 
+            // el.style.backgroundColor = "Red";
+            const rgb = DEFAULT_COLOR_PALLETE[color];
+            const r = rgb[0];
+            const g = rgb[1];
+            const b = rgb[2];
+            // el.style.backgroundColor = 'rgb(' + [r,g,b].join(',') + ')';
+            el.style.setProperty("--pad-color", `rgb(${r},${g},${b})`);
+        }
     }
 }
 
@@ -124,13 +214,33 @@ ws.onmessage = (ev) => {
     packet = { header: null, data: null, footer: null };
 };
 
+// -------- WebSocket send callbacks ---------
 function sendBtn(id,state){
-    // ws.send(new Uint8Array([0x10,id,state]));
+    let header = new Uint8Array(8);
+    header[0] = 2; //command
+    header[1] = 2; //payload length
+    header[2] = 0; header[3] = 0; header[4] = 0; 
+    header[5] = 0; header[6] = 0; header[7] = 0;
+    let payload = new Uint8Array(2);
+    payload[0] = id;
+    payload[1] = state;
+    ws.send(header);
+    ws.send(payload);
     console.log("Button %d state %d", id, state);
 }
 
 function sendPad(id, state) {
-    //state 0 - released, 1 - pressed
+    let header = new Uint8Array(8);
+    header[0] = 6; //command
+    header[1] = 2; //payload length
+    header[2] = 0; header[3] = 0; header[4] = 0;
+    header[5] = 0; header[6] = 0; header[7] = 0;
+
+    let payload = new Uint8Array(2);
+    payload[0] = id;
+    payload[1] = state;
+    ws.send(header);
+    ws.send(payload);
     console.log("Pad %d state %d", id, state);
 }
 
